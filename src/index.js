a80c24b19bf718f295b3e3cbba6fd09a1e27d995
Jose Quilarque <josequiloz@gmail.com>
2022-02-10 22:35:03 -0400

    Practicing react

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const user = {
+    firstName : 'Jose',
+    lastName : 'Quilarque',
+    avatar : 'La leyenda de aang'
+};
+
+
+function getName(user){
+    return  `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user){
+ if(user){
+     return  <h1>Hello {getName(user)}</h1>
+ }
+
+ return <h1>Hello Stranger</h1>
+}
+
+const name = 'Jose';
+const element = <div>{getGreeting(user)} </div>
+const container = document.getElementById('root');
+// reactDOM.render(_QUE_,_DONDE);
+
+
+ReactDOM.render(element, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
