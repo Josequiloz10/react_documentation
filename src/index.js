ba4e88ba7a10b176079450989f035ca340471559
Jose Quilarque <josequiloz@gmail.com>
2022-02-21 23:53:40 -0400

    Component

--- src/index.js
+++ src/index.js
@@ -3,35 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
-const user = {
-    firstName : 'Jose',
-    lastName : 'Quilarque',
-    avatar : 'La leyenda de aang'
-};
-
-
-function getName(user){
-    return  `${user.firstName} ${user.lastName}`
-}
-
-function getGreeting(user){
- if(user){
-     return  <h1>Hello {getName(user)}</h1>
- }
-
- return <h1>Hello Stranger</h1>
-}
-
-const name = 'Jose';
-const element = <div>{getGreeting(user)} </div>
 const container = document.getElementById('root');
+
 // reactDOM.render(_QUE_,_DONDE);
-
-
-ReactDOM.render(element, container);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Card />, container);
