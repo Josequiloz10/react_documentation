65a944048300f56d156c96bda013fbaf6358f30c
Jose Quilarque <josequiloz@gmail.com>
2022-02-24 13:08:45 -0400

    Using API

--- src/pages/ExcercisesNew.js
+++ src/pages/ExcercisesNew.js
@@ -1,87 +1,44 @@
 import React from 'react'
+import ExcerciseForm from './../components/ExcerciseForm'
+import Card from './../components/Card'
 
-class ExcercisesNew extends React.Component {
-
-    state = {};
-
-    render() {
-        return (
-            <div className="container">
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Title"
-                            name="title"
-                            onChange={this.handleChange}
-                            value={this.state.title}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Description"
-                            name="description"
-                            onChange={this.handleChange}
-                            value={this.state.description}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Img"
-                            name="img"
-                            onChange={this.handleChange}
-                            value={this.state.img}
-                        />
-                    </div>
-                    <div className="form-row">
-                        <div className="col">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="LeftColor"
-                                name="leftColor"
-                                onChange={this.handleChange}
-                                value={this.state.leftColor}
-                            />
-                        </div>
-                        <div className="col">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="RightColor"
-                                name="rightColor"
-                                onChange={this.handleChange}
-                                value={this.state.rightColor}
-                            />
-                        </div>
-                    </div>
-
-                    <button
-                        type="submit"
-                        className="btn btn-primary"
-                    >
-                        Submit
-                    </button>
-                </form>
-            </div>
-        )
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-        console.log(this.state)
-    }
+class ExcerciseNew extends React.Component {
+    
+    state = {
+        form :{
+            title: '',
+            description: '',
+            img: '',
+            leftColor: '',
+            rightColor: ''
+        }
+    };
 
     handleChange = e => {
         this.setState({
-            [e.target.name] : e.target.value
+            form: {
+                ...this.state.form, [e.target.name]: e.target.value
+            }
         });
+    }
+
+    render() {
+        return (
+            <div className="row">
+                    <div className="col-sm">
+                        <Card
+                            {...this.state.form}
+                        />
+                    </div>
+                    <div className="col-sm">
+                        <ExcerciseForm
+                            onChange={this.handleChange}
+                            form ={this.state.form}
+                        />
+                    </div>
+            </div>
+        )
     }
 }
 
-export default ExcercisesNew
+export default ExcerciseNew
